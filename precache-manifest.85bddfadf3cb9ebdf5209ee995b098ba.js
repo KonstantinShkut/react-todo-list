43232f29698885a33e1a9c4f4b078a4852cfc04e
Konstantin Shkut <Konstantin.Shkut@gmail.com>
2020-05-21 17:32:45 -0700

    Updates

---
+++ precache-manifest.85bddfadf3cb9ebdf5209ee995b098ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb0e938836c9e702fb6872167f14ac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c89931658b753a881815",
+    "url": "/static/css/2.94ad3b50.chunk.css"
+  },
+  {
+    "revision": "d68048f1f90caeda11a2",
+    "url": "/static/css/main.4430450e.chunk.css"
+  },
+  {
+    "revision": "c89931658b753a881815",
+    "url": "/static/js/2.88c81606.chunk.js"
+  },
+  {
+    "revision": "4e8d24b2339bfb46ff7da8bf8bd2598d",
+    "url": "/static/js/2.88c81606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68048f1f90caeda11a2",
+    "url": "/static/js/main.889236ae.chunk.js"
+  },
+  {
+    "revision": "fe125708d8d6ef6fb147",
+    "url": "/static/js/runtime-main.e7a8ae39.js"
+  }
+]);
